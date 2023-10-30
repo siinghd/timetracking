2809664acd306e97cee328580d6c6813acb69b1e
Singh <h.singh8149@gmail.com>
2023-10-30 11:07:09 +0100

    modified tooltip

--- app/components/Charts.tsx
+++ app/components/Charts.tsx
@@ -17,7 +17,26 @@ const Charts: React.FC<IProps> = ({ data }) => {
   if (!data) {
     return <p>No data available</p>;
   }
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: {
+    active?: any;
+    payload?: any;
+    label?: any;
+  }) => {
+    if (active) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`Name : ${label}`}</p>
+          <p className="intro">{`Hours : ${payload[0].value}`}</p>
+        </div>
+      );
+    }
 
+    return null;
+  };
   return (
     <ResponsiveContainer width="100%" height={250}>
       <BarChart
@@ -26,7 +45,7 @@ const Charts: React.FC<IProps> = ({ data }) => {
       >
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip />} />
 
         <Bar dataKey="hours" fill="#8884d8" radius={[5, 5, 0, 0]} />
       </BarChart>
